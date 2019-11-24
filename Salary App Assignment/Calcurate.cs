f88e5eac9724465ce614f5dec5a51e9a52563473
ngogafique3 <58148646+ngogafique3@users.noreply.github.com>
2019-11-24 23:57:46 +0200

    Add files via upload

---
+++ Salary App Assignment/Calcurate.cs
@@ -0,0 +1,30 @@
+﻿namespace Salary_App_Assignment
+{
+    public class Calcurate
+    {
+        public int EmployeeId;
+        public string EmployeeName;
+        public string EmployeePosition;
+        public string EmployeeGender;
+        
+        public double CalculateMonthlySalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - (((grossSalary * 30) / 100) + ((grossSalary * 5) / 100) + ((grossSalary * 3) / 100));
+            return salary;
+        }
+        public double CalculateHourlySalary(double rate, double hour)
+        {
+            double salary;
+            salary=(rate*hour)-((((rate*hour)*5)/100)+(((rate*hour)*3)/100));
+            return salary;
+        }
+
+        public double CalculateOneTimeSalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - ((grossSalary * 30) / 100);
+            return salary;
+        }
+    }
+}
---
+++ Salary App Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Salary_App_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grossSalary;
+            double rate;
+            int hours;
+            Calcurate emp = new Calcurate();
+            Console.WriteLine("Enter Employee id");
+            emp.EmployeeId= Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Enter Employee Name");
+            emp.EmployeeName= Console.ReadLine();
+           Console.WriteLine("Enter Employee Position");
+            emp.EmployeePosition= Console.ReadLine();
+           Console.WriteLine("Enter Employee Gender");
+            emp.EmployeeGender= Console.ReadLine();
+            Console.WriteLine("Enter Employee Gross Salary");
+            grossSalary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The monthly salary of {emp.EmployeeName} with id {emp.EmployeeId} on {emp.EmployeePosition} Position is {emp.CalculateMonthlySalary(grossSalary)} ");
+            Console.WriteLine($"The one time salary of {emp.EmployeeName} with id {emp.EmployeeId} on {emp.EmployeePosition} Position is {emp.CalculateOneTimeSalary(grossSalary)} ");
+            Console.WriteLine("Enter Employee Rate");
+            rate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Employee Working Hours");
+            hours = Convert.ToInt32(Console.ReadLine());
+            if (rate != 0 && hours != 0)
+            {
+                Console.WriteLine($"The one time salary of {emp.EmployeeName} with id {emp.EmployeeId} on {emp.EmployeePosition} Position is {emp.CalculateHourlySalary(rate,hours)} ");
+            }
+            else
+            {
+                Console.WriteLine("The one time salary of {emp.EmployeeName} with id {emp.EmployeeId} on {emp.EmployeePosition} Position is 0 ");
+            }
+
+        }
+    }
+}
